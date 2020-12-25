6703cb10d31df105a10b929abe66513d56ccad89
ankit-sinha-18 <72637068+ankit-sinha-18@users.noreply.github.com>
2020-12-25 23:27:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-    kuttit_api_key: process.env.kuttit_api_key || "",
-    bot_api_key: process.env.bot_api_key || ""
+    kuttit_api_key: process.env.kuttit_api_key || "E5YX2zICLNXCY0aT4eJdK4lwxlOAkTTtozyV_T4C",
+    bot_api_key: process.env.bot_api_key || "1420077711:AAHbcwP0N2b6CqgNjm8WMnbqW5WTY1LXSVk"
 };
